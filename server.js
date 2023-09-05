b48208c1f7cf5226efdce0b599c5b10bded473c0
siemhesda <143130929+siemhesda@users.noreply.github.com>
2023-09-05 13:36:31 -0700

    add env port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import fs from "fs";
 import path from "path";
 
 const app = express();
-const PORT = 3001; // You can use any port you prefer
+const PORT = process.env.PORT || 3001; // You can use any port you prefer
 
 app.use(express.json());
 app.use(cors()); // Allow cross-origin requests
