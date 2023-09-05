eec6037b9ffdfcaa180fe2c76ca0457d9d143da8
siemhesda <143130929+siemhesda@users.noreply.github.com>
2023-09-05 13:58:20 -0700

    add test

--- server.js
+++ server.js
@@ -39,6 +39,10 @@ app.delete("/api/notes", (req, res) => {
   res.status(201).json({ message: "Note deleted successfully" });
 });
 
+app.get("/api/version", (req, res) => {
+  res.status(200).json({ message: "this is to test the pipeline", value: 1 });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
